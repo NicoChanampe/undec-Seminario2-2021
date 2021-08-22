ee70b71f64c83616019e2d0b6041077fa0e6aae8
NicoChanampe <champis_97@hotmail.com.ar>
2021-08-21 23:31:55 -0300

    TPI[Se creo y abrio el proyecto]

---
+++ pandulce/src/main/java/com/undec/pandulce/PandulceBackendClasesApplication.java
@@ -0,0 +1,13 @@
+package com.undec.pandulce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandulceBackendClasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandulceBackendClasesApplication.class, args);
+	}
+
+}
---
+++ pandulce/src/test/java/com/undec/pandulce/PandulceBackendClasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undec.pandulce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandulceBackendClasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
