179d48ed0466f5811edf9eb6eb8ecdf884bbde42
NicoChanampe <champis_97@hotmail.com.ar>
2021-08-29 20:05:35 -0300

    TPI[Se creo el metodo get en PanController]

--- pandulce/src/main/java/com/undec/pandulce/controller/PanController.java
+++ pandulce/src/main/java/com/undec/pandulce/controller/PanController.java
@@ -4,6 +4,7 @@ import com.undec.pandulce.dto.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,10 @@ public class PanController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> get(@PathVariable String id){
+        Response response = new Response();
+        response.setStatus(1);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
