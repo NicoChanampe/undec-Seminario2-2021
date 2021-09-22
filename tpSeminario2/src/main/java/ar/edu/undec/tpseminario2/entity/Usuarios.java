9cc1183d058ae8d50c9b396ee0cdbbbb6cff14ac
NicoChanampe <champis_97@hotmail.com.ar>
2021-09-22 12:06:37 -0300

    TPI[cambios en la clase Usuarios]

--- tpSeminario2/src/main/java/ar/edu/undec/tpseminario2/entity/Usuarios.java
+++ tpSeminario2/src/main/java/ar/edu/undec/tpseminario2/entity/Usuarios.java
@@ -1,6 +1,10 @@
 package ar.edu.undec.tpseminario2.entity;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
 import java.util.Objects;
 
 @Entity
@@ -13,10 +17,13 @@ public class Usuarios {
     private String telefono;
     private String mailDeCuenta;
     private String contraseña;
+    private Byte estado;
+    private Date fechacreacion;
+    private Date fechamodificacion;
+    private Date fechadebaja;
 
     @Id
     @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer getId() {
         return id;
     }
@@ -95,16 +102,56 @@ public class Usuarios {
         this.contraseña = contraseña;
     }
 
+    @Basic
+    @Column(name = "estado", nullable = false)
+    public Byte getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Byte estado) {
+        this.estado = estado;
+    }
+
+    @Basic
+    @Column(name = "fechacreacion", nullable = true)
+    public Date getFechacreacion() {
+        return fechacreacion;
+    }
+
+    public void setFechacreacion(Date fechacreacion) {
+        this.fechacreacion = fechacreacion;
+    }
+
+    @Basic
+    @Column(name = "fechamodificacion", nullable = true)
+    public Date getFechamodificacion() {
+        return fechamodificacion;
+    }
+
+    public void setFechamodificacion(Date fechamodificacion) {
+        this.fechamodificacion = fechamodificacion;
+    }
+
+    @Basic
+    @Column(name = "fechadebaja", nullable = true)
+    public Date getFechadebaja() {
+        return fechadebaja;
+    }
+
+    public void setFechadebaja(Date fechadebaja) {
+        this.fechadebaja = fechadebaja;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Usuarios usuarios = (Usuarios) o;
-        return Objects.equals(id, usuarios.id) && Objects.equals(nombre, usuarios.nombre) && Objects.equals(apellido, usuarios.apellido) && Objects.equals(dni, usuarios.dni) && Objects.equals(direccion, usuarios.direccion) && Objects.equals(telefono, usuarios.telefono) && Objects.equals(mailDeCuenta, usuarios.mailDeCuenta) && Objects.equals(contraseña, usuarios.contraseña);
+        return Objects.equals(id, usuarios.id) && Objects.equals(nombre, usuarios.nombre) && Objects.equals(apellido, usuarios.apellido) && Objects.equals(dni, usuarios.dni) && Objects.equals(direccion, usuarios.direccion) && Objects.equals(telefono, usuarios.telefono) && Objects.equals(mailDeCuenta, usuarios.mailDeCuenta) && Objects.equals(contraseña, usuarios.contraseña) && Objects.equals(estado, usuarios.estado) && Objects.equals(fechacreacion, usuarios.fechacreacion) && Objects.equals(fechamodificacion, usuarios.fechamodificacion) && Objects.equals(fechadebaja, usuarios.fechadebaja);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nombre, apellido, dni, direccion, telefono, mailDeCuenta, contraseña);
+        return Objects.hash(id, nombre, apellido, dni, direccion, telefono, mailDeCuenta, contraseña, estado, fechacreacion, fechamodificacion, fechadebaja);
     }
 }
